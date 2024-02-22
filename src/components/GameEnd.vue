01c463bd3b55324669275cdf233af9a6ce11de38
Naamee <naameeakram@gmail.com>
2024-02-22 15:14:22 +0500

    added win notification box

--- src/components/GameEnd.vue
+++ src/components/GameEnd.vue
@@ -1,5 +1,7 @@
 <template>
-    <div class="flex items-center justify-center bg-black">
-        <p class="text-white">frfrfrfr</p>
+    <div class="flex flex-col items-center justify-center bg-gradient-to-r from-indigo-900/90 to-indigo-800/90 py-8 px-24 border border-gray-100/25">
+        <p class="text-emerald-100 text-2xl">You Win!!</p>
+        <!-- <p class="text-white">Wow! You Lost!?</p> -->
+        <button class="text-white/50 text-sm mt-2 animate-pulse hover:underline hover:animate-none hover:text-white">Play Again?</button>
     </div>
 </template>
